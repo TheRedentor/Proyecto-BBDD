a185f6356236b6cafedcf2e1b219b33e38aa2611
Isaac0312 <isaacejarque@hotmail.com>
2022-11-18 08:56:25 +0100

    Añadido proyecto

---
+++ Chat4Play/src/main/java/com/chat/play/Chat4PlayApplication.java
@@ -0,0 +1,13 @@
+package com.chat.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat4PlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chat4PlayApplication.class, args);
+	}
+
+}
---
+++ Chat4Play/src/main/java/com/chat/play/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.chat.play;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Chat4PlayApplication.class);
+	}
+
+}
