b648dfe0e9bb86a17ddc7260d40379833db1d737
Isaac0312 <isaacejarque@hotmail.com>
2022-11-18 10:27:48 +0100

    Update party con mensaje

--- Chat4Play/src/main/java/com/chat/play/dto/Party.java
+++ Chat4Play/src/main/java/com/chat/play/dto/Party.java
@@ -35,15 +35,20 @@ public class Party {
 	@JoinColumn(name="id_videojuego")
 	private Videojuego videojuego;
 	
+	@OneToMany
+	@JoinColumn(name="id_mensaje")
+	private List<Mensaje> mensaje;
+	
 	public Party() {
 		
 	}
 	
-	public Party(String nombre, Long id, List<Usuario> usuario, Videojuego videojuego) {
+	public Party(String nombre, Long id, List<Usuario> usuario, Videojuego videojuego, List<Mensaje> mensaje) {
 		this.id_party=id;
 		this.nombre=nombre;
 		this.usuario=usuario;
 		this.videojuego=videojuego;
+		this.mensaje=mensaje;
 	}
 
 	/**
@@ -88,6 +93,22 @@ public class Party {
 	 */
 	public void setUsuario(List<Usuario> usuario) {
 		this.usuario = usuario;
+	}
+
+	/**
+	 * @return the mensaje
+	 */
+	public List<Mensaje> getMensaje() {
+		return mensaje;
+	}
+
+	/**
+	 * @param mensaje the mensaje to set
+	 */
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Mensaje")
+	public void setMensaje(List<Mensaje> mensaje) {
+		this.mensaje = mensaje;
 	}
 
 	/**
