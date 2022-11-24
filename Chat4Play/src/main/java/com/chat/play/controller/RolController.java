de7ff2ee23e0a8b80ee764879e195b93a6a63aae
Isaac0312 <isaacejarque@hotmail.com>
2022-11-24 13:03:40 +0100

    RequestBody Rol

--- Chat4Play/src/main/java/com/chat/play/controller/RolController.java
+++ Chat4Play/src/main/java/com/chat/play/controller/RolController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import com.chat.play.service.RolServiceImpl;
 @RestController
 @RequestMapping("/api")
 public class RolController {
+	
 	@Autowired
 	RolServiceImpl rolServiceImpl;
 	
@@ -27,7 +29,7 @@ public class RolController {
 	
 	
 	@PostMapping("/roles")
-	public Rol salvarRol(Rol rol) {
+	public Rol salvarRol(@RequestBody Rol rol) {
 		
 		return rolServiceImpl.guardarRol(rol);
 	}
@@ -46,7 +48,7 @@ public class RolController {
 	}
 	
 	@PutMapping("/roles/{id_rol}")
-	public Rol actualizarRol(@PathVariable(name="id_rol")Long id_rol, Rol rol) {
+	public Rol actualizarRol(@PathVariable(name="id_rol")Long id_rol,@RequestBody  Rol rol) {
 		
 		Rol rol_seleccionado= new Rol();
 		Rol rol_actualizado= new Rol();
